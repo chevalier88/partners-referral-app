04794f23e5d0f503e14d1907a9f3fbabb5c5a11a
Graham <graham.lim88@gmail.com>
2022-05-16 23:31:48 +0800

    login Form component placeholders done

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,14 @@
-import axios from 'axios';
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-
-// we have to build the form first
-function BillForm({ bills, setBills }) {
-  // Control the form input value using a state variable name.
-  // here we pass the state INTO the component params because it ends up becoming impacted by axios
-  // axios will change the hook state using setBills to whatever bills will now be
-  // this in turn produces 
-
-  const handleSubmit = (event) => {
-    event.preventDefault(); // prevents refreshing?
-    console.log('asdsa');
-    console.log(event.target.Bill.value);
-
-    axios.post('/bill', {
-      name: event.target.Bill.value,
-    })
-      .then((response) => {
-        console.log(response.data);
-        const newBills = [...bills];
-        newBills.push(event.target.Bill.value);
-        setBills(newBills);
-      });
-  };
-  return (
-    <div>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3">
-          <Form.Label>Bill</Form.Label>
-          <Form.Control placeholder="Bill input" name="Bill" />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-
-      </Form>
-      <br />
-      <br />
-      Bills So Far:
-      {' '}
-      <ul>
-        {bills.map((bill) => (
-          <li key={bill.toString()}>
-            {bill}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-// form shower will spawn the initial form
-function FormShower({ bills, setBills }) {
-  const [showForm, setShowForm] = useState(false);
-
-  const showFormHandler = (event) => {
-    event.preventDefault();
-    axios
-      .get('/bills')
-      .then((response) => {
-        console.log('receiving data from db after findAll bills...');
-        console.log(response.data);
-        const parseableArrayOfBillStrings = response.data.map((({ name }) => name));
-        console.log(parseableArrayOfBillStrings);
-        setBills(parseableArrayOfBillStrings);
-      })
-      .catch((error) => console.log(error));
-    setShowForm(true);
-    console.log('refresh prevented');
-  };
-
-  return (
-    <div>
-    </div>
-  );
-}
+import Login from './components/Login.jsx';
 
 export default function App() {
-  const [bills, setBills] = useState([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div>
-      <FormShower bills={bills} setBills={setBills} />
+      {/* <FormShower bills={bills} setBills={setBills} /> */}
+      <Login email = {email} setEmail = {setEmail} password = {password} setPassword = {setPassword}/>
     </div>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
-export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+export default function Login({email, setEmail, password, setPassword}) {
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
@@ -12,32 +11,37 @@ export default function Login() {
 
   function handleSubmit(event) {
     event.preventDefault();
+    console.log(email);
+    console.log(password);
   }
 
   return (
     <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-      </Form>
+      <Container>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group size="sm-3" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              autoFocus
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group size="sm-3" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <br></br>
+          <Button block size="sm-3" type="submit" disabled={!validateForm()}>
+            Login
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 }
