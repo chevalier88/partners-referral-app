872a274c3b489f7eebe784890226a8d067edce0d
Graham <graham.lim88@gmail.com>
2022-05-20 23:00:01 +0800

    request form dropdown menu functionality works, continuing to build form

--- src/components/RequestForm.jsx
+++ src/components/RequestForm.jsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 import axios from "axios";
 
 export default function RequestForm() {
   const [serviceRequested, setServiceRequested] = useState("");
-  const [regionsRequested, setRegionsRequested] = useState([]);
+  const [regionRequested, setRegionRequested] = useState([]);
   const [entitiesStatus, setEntitiesStatus] = useState("");
   const [employeeNumbers, setEmployeeNumbers] = useState(0);
   const [comments, setComments] = useState("");
@@ -28,13 +30,25 @@ export default function RequestForm() {
                   onChange={(e) => setServiceRequested(e.target.value)}
                 />
               </Form.Group>
-              <Form.Group size="sm-3" controlId="regionsRequested">
-                <Form.Label>Region(s) Requested:</Form.Label>
+              <Form.Group controlId="regionRequested">
+                <Form.Label>Region Requested:</Form.Label>
                 <Form.Control
-                  type="regionsRequested"
-                  value={regionsRequested}
-                  onChange={(e) => setRegionsRequested(e.target.value)}
-                />
+                  as="select"
+                  value={regionRequested}
+                  onChange={e => {
+                    console.log("e.target.value", e.target.value);
+                    setRegionRequested(e.target.value);
+                  }}
+                >
+                  <option value="1">Southeast Asia (ASEAN)</option>
+                  <option value="2">Japan</option>
+                  <option value="3">Korea</option>
+                  <option value="4">Hong Kong and Greater Bay Area (HK GBA)</option>
+                  <option value="5">Australia and New Zealand (ANZ)</option>
+                  <option value="6">Asia Pacific (APAC)</option>
+                  <option value="7">Europe, Middle East and Africa (EMEA)</option>
+                  <option value="8">North America (NA)</option>
+                </Form.Control>
               </Form.Group>
               <br></br>
               <Button block size="sm-3" type="submit" >
