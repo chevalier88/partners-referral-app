4c1851fd2308f1c3d79aa77a840074ad811c4d15
Graham <graham.lim88@gmail.com>
2022-05-26 20:46:13 +0800

    debugging main request form

--- controllers/request.mjs
+++ controllers/request.mjs
@@ -8,20 +8,34 @@ export default function initRequestController(db) {
       console.log('regions_id:')
       console.log(regions_id);
       
-      const newRequest = await db.Request.create({
-        referring_employee_id,
+      const newRequestObject = {
+        referring_employee_id: referring_employee_id,
         employee_numbers: Number(employee_numbers),
-        entities_existing,
-        comments,
-        request_addressed,
+        entities_existing: entities_existing,
+        comments: comments,
+        request_addressed: request_addressed,
         services_id: Number(services_id),
-      });
+        partner_id: null,
+        // created_at: new Date(),
+        // updated_at: new Date(),
+      };
 
+      console.log('printing newRequestObject:');
+      console.log(newRequestObject);
+      const newRequest = await db.Request.create(newRequestObject);
+      console.log('do we even get here?');
+      console.log(newRequest);
       const { id } = newRequest;
-
       console.log(id);
-      // await request.body.newRequest.id
-      // response.send( requestSubmitted );
+
+      // db.Request.findByPk(id).then( async(request) => {
+      //   try{
+      //     await request.setRegions(regions_id); // 3 relationship are in db 
+      //   } catch(e){
+      //     console.error(e);
+      //   }
+      // });
+      response.send( 200 );
     } catch (error) {
       console.log(error);
       response.send(error);
